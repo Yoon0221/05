687eba7d71fa3ea96b84aa8e4b14632e9f32b90d
신지윤 <sin020511@gmail.com>
2022-10-04 16:40:06 +0900

    2

--- main.c
+++ main.c
@@ -10,18 +10,14 @@ int main(int argc, char *argv[])
 	printf("정수 하나를 입력하시오. : ");
 	scanf("%d", &num);
 	
-	if (num>0)
+	if (num >=0)
 	{
-		printf("양수입니다. \n");
-	}
-	else if (num<0)
-	{
-		printf("음수입니다. \n");
+		printf("절대값은 %i입니다. \n", num);
 	}
 	else
 	{
-		printf("0입니다. \n");
-	} 
+		printf("절대값은 %i입니다. \n", -num);
+	}
 	
 	return 0;
 }
